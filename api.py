08f94e5b7eed359374278b82e6a5df463c1df7e3
Adlok53 <01mikinini@gmail.com>
2024-09-02 17:49:56 +0300

    environment id ready

--- api.py
+++ api.py
@@ -1,7 +1,12 @@
 import telebot
-import environ
+import os
+from dotenv import load_dotenv, dotenv_values
 
-bot = telebot.TeleBot(env('TG_TOKEN'))
+load_dotenv()
+
+API_TOKEN = os.getenv("TOKEN")
+
+bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
